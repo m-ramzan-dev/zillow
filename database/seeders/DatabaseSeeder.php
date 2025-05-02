255dbc1c20a1949c261cb553818fa08a5544a6de
Ramzan <mhdramzan.dev@gmail.com>
2025-05-02 18:17:20 +0500

    listing migration -- model -- factory -- seeder classes added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        \App\Models\Listing::factory(20)->create();
 
         User::factory()->create([
             'name' => 'Test User',
