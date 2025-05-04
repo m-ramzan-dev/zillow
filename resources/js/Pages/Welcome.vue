823d683d1842e5de293b4c2bb4043d7b7c92c6f2
Ramzan <mhdramzan.dev@gmail.com>
2025-05-04 12:48:40 +0500

    inertia not working

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,10 +1,10 @@
 <script setup>
-defineProps(["name"]);
+//defineProps(["name"]);
 </script>
 
 <template>
   <div>
     <h1>Welcome</h1>
-    <p>Hello {{ name }}, welcome to your first Inertia app!</p>
+    <p>Hello Ramzan, welcome to your first Inertia app!</p>
   </div>
 </template>
