e37aaa9dc8a2264a2ec57acbd4f20d27757f470c
Ramzan <mhdramzan.dev@gmail.com>
2025-05-15 20:46:02 +0500

    header menu added

--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -2,19 +2,18 @@
 import { Link } from "@inertiajs/vue3";
 </script>
 
-<!-- <script>
+<script>
 export default {
   layout: (h, page) => h(Layout, [page]),
-
-  layout: Layout,
 };
-</script> -->
+</script>
 <template>
   <main>
     <header>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
+      <Link href="/listings">Welcome</Link>
     </header>
   </main>
   <slot></slot>
