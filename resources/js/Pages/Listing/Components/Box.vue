095c3911dcc0d6f25de4cd5cbebdcd80eb2932c9
Ramzan <mhdramzan.dev@gmail.com>
2025-05-17 18:45:31 +0500

    show view styled with price calculator

---
+++ resources/js/Pages/Listing/Components/Box.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="border rounded-md shadow-sm border-gray-200 dark:border-gray-800 dark:text-gray-300 p-2"
+  >
+    <div class="font-medium text-gray-500 mb-1">
+      <slot name="header"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,13 +1,69 @@
 <script setup>
 import ListItem from "./ListItem.vue";
-defineProps({
+import Box from "./Components/Box.vue";
+import { ref, computed } from "vue";
+const props = defineProps({
   listing: {
     type: Object,
     required: true,
   },
 });
+const interestRate = ref(2.5);
+const duration = ref(25);
+const monthlyPayment = computed(() => {
+  const principal = props.listing.price;
+  const rate = interestRate.value / 100 / 12;
+  const n = duration.value * 12;
+  return ((principal * rate) / (1 - Math.pow(1 + rate, -n))).toFixed(1);
+});
 </script>
 <template>
   <div>Showing Single Listing</div>
-  <ListItem :listing="listing" />
+  <div class="grid grid-cols-6 gap-4">
+    <div class="col-span-4">
+      <Box
+        ><div class="w-full text-center p-4">
+          <span>No Images</span>
+        </div></Box
+      >
+    </div>
+    <div class="col-span-2 space-y-4">
+      <ListItem :listing="props.listing" />
+      <div>
+        <Box>
+          <template #header>Monthly Payment</template>
+          <div>
+            <label for="interest" class="label"
+              >Interest rate ({{ interestRate }}%)</label
+            >
+            <input
+              v-model.number="interestRate"
+              type="range"
+              min="0.1"
+              max="30"
+              step="0.1"
+              value="5"
+              class="w-full h-4 bg-gray-200 dark:bg-gray-700 rounded-lg appearance-none"
+            />
+            <label for="duration" class="label"
+              >Duration ({{ duration }} years)</label
+            >
+            <input
+              v-model.number="duration"
+              type="range"
+              min="3"
+              max="35"
+              step="1"
+              value="5"
+              class="w-full h-4 bg-gray-200 dark:bg-gray-700 rounded-lg appearance-none"
+            />
+            <div class="text-gray-600 dark:text-gray-300 mt-2">
+              <div class="text-gray-400">Your monthly payment</div>
+              <div class="text-3xl">${{ monthlyPayment }}</div>
+            </div>
+          </div>
+        </Box>
+      </div>
+    </div>
+  </div>
 </template>
