3b70efe7ee9557cbe9ce039e24e588b772829e20
Ramzan <mhdramzan.dev@gmail.com>
2025-05-26 13:11:41 +0500

    filters menue added at the top of website

---
+++ resources/js/Pages/Listing/Components/Filters.vue
@@ -0,0 +1,37 @@
+<template>
+  <form>
+    <div class="flex flex-wrap mb-4 mt-2 gap-2">
+      <div class="flex flex-nowrap items-center">
+        <input
+          type="text"
+          placeholder="Price from"
+          class="input-filter-l w-28"
+        />
+        <input type="text" placeholder="Price to" class="input-filter-r w-28" />
+      </div>
+      <div class="flex flex-nowrap items-center">
+        <select name="beds" id="beds" class="input-filter-l w-28">
+          <option :value="null">Beds</option>
+          <option v-for="n in 5" :value="n" :key="n">{{ n }}</option>
+          <option>6+</option>
+        </select>
+        <select name="baths" id="baths" class="input-filter-r w-28">
+          <option :value="null">Baths</option>
+          <option v-for="n in 5" :value="n" :key="n">{{ n }}</option>
+          <option>6+</option>
+        </select>
+      </div>
+      <div class="flex flex-nowrap items-center">
+        <input
+          type="text"
+          placeholder="Area from"
+          class="input-filter-l w-28"
+        />
+        <input type="text" placeholder="Area to" class="input-filter-r w-28" />
+      </div>
+      <button type="submit" class="btn-normal">Filter</button>
+      <button type="reset">Clear</button>
+    </div>
+  </form>
+</template>
+<script setup></script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,6 +2,7 @@
 import { Link } from "@inertiajs/vue3";
 import ListItem from "./ListItem.vue";
 import Pagination from "./Components/Pagination.vue";
+import Filters from "./Components/Filters.vue";
 defineProps({
   listings: {
     type: Object,
@@ -11,6 +12,7 @@ defineProps({
 </script>
 <template>
   <div>All Showing Listings</div>
+  <Filters />
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
     <div v-for="listing in listings.data" :key="listing.id">
       <Link :href="`/listings/${listing.id}`">
